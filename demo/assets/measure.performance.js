9cddc394433fe8e444c909d8986df66d1f17eb57
Alex <sawenok@gmail.com>
2020-02-03 10:38:22 +0200

    fix

--- demo/assets/measure.performance.js
+++ demo/assets/measure.performance.js
@@ -1,9 +1,5 @@
-document.getElementById('title').innerHTML =
-  window.performance.getEntries()[0].nextHopProtocol && window.performance.getEntries()[0].nextHopProtocol === 'h2'
-    ? 'http/2'
-    : 'http/1.1';
-
 const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || null;
+
 if (connection) {
   let speedType = 'fast';
   if (/\slow-2g|2g|3g/.test(connection.effectiveType)) {
@@ -19,7 +15,7 @@ if ('PerformanceObserver' in window) {
   const observer = new PerformanceObserver(list => {
     const entries = list.getEntries();
     entries.map(res => {
-      if (res.initiatorType === 'img') {        
+      if (res.initiatorType === 'img') {
         performanceData.push({
           name: res.name,
           startTime: res.startTime,
@@ -34,8 +30,6 @@ if ('PerformanceObserver' in window) {
 }
 
 window.addEventListener('load', () => {
-  document.getElementById('switchBtn').style.display = 'block';
-
   // Calculate avg. load duration
   let avgLoadDuration = (performanceData.reduce((r, c) => r + c.duration, 0) / performanceData.length / 1000).toFixed(
     3
@@ -44,6 +38,7 @@ window.addEventListener('load', () => {
   // Calculate total images loading time
   let responseEnd = (performanceData.reduce((r, c) => Math.max(r, c.responseEnd), 0) / 1000).toFixed(3);
 
+  document.getElementById('switchBtn').style.display = 'block';
   document.getElementById('performance').innerHTML = `<strong>Avg. load duration:</strong> ${avgLoadDuration} sec<br>
     <strong>All images loaded within:</strong> <strong style='color:red'>${responseEnd} sec.</strong>`;
 });
